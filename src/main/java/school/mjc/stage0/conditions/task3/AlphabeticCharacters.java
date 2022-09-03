835fb475b5d7c4cd020a5e103265216e50e7ecd5
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-03 15:16:41 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        int code=character;
+        if((code>=65 && code<=90) || (code>=97 && code<=122)){
+            if (character=='A' || character=='E' || character=='I'
+            || character=='O' || character=='U' || character=='Y' || character=='a' || character=='e' || character=='i'
+                    || character=='o' || character=='u' || character=='y')
+                System.out.println("Vowel");
+            else System.out.println("Consonant");
+        }else System.out.println("wrong alphabet!");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        int code=symbol;
+        if((code>=65 && code<=90) || (code>=97 && code<=122)){
+            System.out.println("English");
+        }else System.out.println("Non English");
     }
 }
